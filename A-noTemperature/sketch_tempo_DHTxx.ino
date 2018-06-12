e56283198024fd3450a4dc9a1f01c9500746e8a4
Alexey Popov <39190317+alexeyinco@users.noreply.github.com>
2018-06-12 21:22:12 +0300

    Update sketch_tempo_DHTxx.ino

--- A-noTemperature/sketch_tempo_DHTxx.ino
+++ A-noTemperature/sketch_tempo_DHTxx.ino
@@ -3,9 +3,9 @@
 #define DHTPIN 2 // номер пина, к которому подсоединен датчик
 
 // Раскомментируйте в соответствии с используемым датчико
-// Инициируем датчик
+// Инициируем датчик. Используем DHT22.
 
-//DHT dht(DHTPIN, DHT22); //если используется датчик dht22 
+DHT dht(DHTPIN, DHT22); //если используется датчик dht22 
 //DHT dht(DHTPIN, DHT11); //если используется датчик dht11 
 
 void setup() {
@@ -17,12 +17,12 @@ dht.begin();
 
 void loop() {
 
-delay(2000);
+delay(2000); // задержка 2 секунды между измеренеями 
 
-float h = dht.readHumidity();   
+float h = dht.readHumidity();    // Считываем влажность
 
-float t = dht.readTemperature();
-
+float t = dht.readTemperature(); // Считываем температуру
+// Проверка удачно прошло ли считывание:
 if (isnan(h) || isnan(t)) {
 
 Serial.println("Не удается считать показания");
@@ -31,6 +31,6 @@ return;
 
 }
 
-Serial.print("Влажность: "+h+" %\t"+"Температура: "+t+" *C ");
+Serial.print("-Humidity: "+h+" %\t"+"-Temperature: "+t+" *C "); // показания
 
 }
