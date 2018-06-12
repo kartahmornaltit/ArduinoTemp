d83b274c3ac8446428c607fd18123d880c61092e
Alexey Popov <39190317+alexeyinco@users.noreply.github.com>
2018-06-12 23:45:51 +0300

    Update sketch_ds18b20_dallas.ino.ino

--- A-noTemperature/sketch_ds18b20_dallas.ino.ino
+++ A-noTemperature/sketch_ds18b20_dallas.ino.ino
@@ -1,35 +1,46 @@
 #include <OneWire.h>
+#include <DallasTemperature.h>
  
-#define PIN_DS18B20 8
+#define PIN_DS18B20 8  // Номер пина Arduino с подключенным датчиком
  
-OneWire oneWire(PIN_DS18B20);
+OneWire oneWire(PIN_DS18B20);  // Аналогично создаем объект OneWire
+
+DallasTemperature dallasSensors(&oneWire); // Создаем объект DallasTemperature для работы с сенсорами, передавая ему ссылку на объект для работы с OneWire.
  
-DeviceAddress sensorAddress;
+DeviceAddress sensorAddress; // Специальный объект для хранения адреса устройства
  
 void setup(void){
   Serial.begin(9600);
-  Serial.println("Пример работы с датчиком температуры ds18b20");
+  Serial.println("Пример работы с датчиком температуры ds18b20 с помощью библиотеки DallasTemperature");
  
   Serial.print("Ищем устройства...");
   dallasSensors.begin();
   Serial.print("Найдено ");
   Serial.print(dallasSensors.getDeviceCount(), DEC);
-  Serial.println(" устройств.");
+  Serial.println(" устройств");
  
-  // Определяем режим питания (по отдельной линии или через паразитное питание по линии данных)
   Serial.print("Режим паразитного питания: ");
   if (dallasSensors.isParasitePowerMode()) Serial.println("ВКЛЮЧЕН");
   else Serial.println("ВЫКЛЮЧЕН");
  
+  sensorAddress = { 0x28, 0x1D, 0x39, 0x31, 0x2, 0x0, 0x0, 0xF0 };  // если хотим задать адрес устройства вручную
+ 
   // Поиск устройства:
   // Ищем адрес устройства по порядку (индекс задается вторым параметром функции)
   if (!dallasSensors.getAddress(sensorAddress, 0)) Serial.println("Не можем найти первое устройство");
-
+ 
+  // 2-ой вариант с помощью OneWire, перезапускаем поиск:
+  oneWire.reset_search();
+ 
+  // Находим первое устройство и запоминаем его адрес в sensorAddress
+  if (!oneWire.search(sensorAddress)) Serial.println("Unable to find address for sensorAddress");
+ 
+  // Отображаем адрес ds18b20, который мы нашли:
   Serial.print("Адрес устройства: ");
   printAddress(sensorAddress);
   Serial.println();
  
-  dallasSensors.setResolution(sensorAddress, 12);
+  dallasSensors.setResolution(sensorAddress, 12); // разрешение датчика в 12 бит (точность уменьшится, скорость данных увеличится)
  
   Serial.print("Разрешение датчика: ");
   Serial.print(dallasSensors.getResolution(sensorAddress), DEC);
