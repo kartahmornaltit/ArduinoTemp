ac405ff393d17986fa45d0a6ba436a6879f4d6ca
kartahmornaltit <st049343@gmail.com>
2018-06-13 03:58:13 +0300

    add comments

--- server/flask_app.py
+++ server/flask_app.py
@@ -6,9 +6,12 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_bootstrap import Bootstrap
 
 
+# Текущая директория
 basedir = path.abspath(path.dirname(__file__))
 
+# Flask приложение
 app = Flask(__name__)
+# Обновление конфигурации
 app.config.update(
     dict(
         SECRET_KEY='powerful-secretkey',
@@ -18,21 +21,26 @@ app.config.update(
     )
 )
 
+# База данных
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+# Bootstrap
 bootstrap = Bootstrap(app)
 
 
+# Модель базы данных
 class Temperature(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     value = db.Column(db.Integer, nullable=False)
     datetime = db.Column(db.DateTime, nullable=False)
 
+    # Получение n последних данных
     @staticmethod
     def get_last_values(n):
         return Temperature.query.order_by(Temperature.datetime.desc())[:n]
 
 
+# Путь для графика в реальном времени
 @app.route('/')
 @app.route('/index')
 @app.route('/realtime')
@@ -41,12 +49,14 @@ def index():
     return render_template('chart.html', values=values)
 
 
+# Путь для графика истории
 @app.route('/history')
 def history():
     values = Temperature.get_last_values(30)
     return render_template('chart.html', values=values)
 
 
+# Путь для добавления данных в базу данных
 @app.route('/add_data', methods=['POST'])
 def add_data():
     data = request.json
@@ -57,4 +67,5 @@ def add_data():
 
 
 if __name__ == "__main__":
+    # Запуск сервера
     app.run()
